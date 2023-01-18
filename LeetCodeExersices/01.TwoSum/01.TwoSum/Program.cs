f71ccdf7711ff047adad460d587449e0ba19055b
IvayloTodorov <rakuuqwas300@gmail.com>
2023-01-18 19:41:46 +0200

    01.TwoSum

---
+++ LeetCodeExersices/01.TwoSum/01.TwoSum/Program.cs
@@ -0,0 +1,27 @@
+﻿var nums = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+var target = int.Parse(Console.ReadLine());
+
+// 2,7,11,15
+
+var arr = new int[2];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int j = 0; j < nums.Length; j++)
+    {
+        if (nums[i] + nums[j] == target)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            arr[0] = i;
+            arr[1] = j;
+            break;
+        }
+    }
+
+}
+
+Console.WriteLine(String.Join(", ", arr));
