a5a296e37e3b894792ffc13f3b89f1caf5e70642
IvayloTodorov <rakuuqwas300@gmail.com>
2023-01-24 19:13:19 +0200

    27.RemoveElement

---
+++ LeetCodeExersices/27.RemoveElement/27.RemoveElement/Program.cs
@@ -0,0 +1,13 @@
+﻿var nums = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+var val = int.Parse(Console.ReadLine());
+
+int j = 0;
+foreach (int i in nums)
+{
+    if (i != val)
+    {
+        nums[j++] = i;
+    }
+}
+
+Console.WriteLine(j);
