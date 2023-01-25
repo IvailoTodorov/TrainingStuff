2cafd9af5ddbe70103a155447e0b5d963c003ee6
IvayloTodorov <rakuuqwas300@gmail.com>
2023-01-25 19:24:44 +0200

    43.MultiplyStrings

---
+++ LeetCodeExersices/43.MultiplyStrings/43.MultiplyStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+
+var first = BigInteger.Parse(num1);
+var second = BigInteger.Parse(num2);
+
+var result = first + second;
+
+Console.WriteLine(result.ToString());
