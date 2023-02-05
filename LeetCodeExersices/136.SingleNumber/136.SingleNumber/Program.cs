78b7e257d1eecc0b4f88fea5307c9bd9bacfeb27
IvayloTodorov <rakuuqwas300@gmail.com>
2023-02-05 19:35:21 +0200

    136.SingleNumber

---
+++ LeetCodeExersices/136.SingleNumber/136.SingleNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿var nums = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+var arr = nums.GroupBy(x => x).Where(g => g.Count() < 2).Select(y => y.Key).ToArray();
+
+Console.WriteLine(String.Join(", ", arr));
