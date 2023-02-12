03ac9b00453bcc148d6d65d41c4c978c9441e0fe
IvayloTodorov <rakuuqwas300@gmail.com>
2023-02-12 16:07:01 +0200

    HackerRank exersice Inheritance

---
+++ HackeRankExersices/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book
+{
+
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+    public abstract void display();
+
+
+}
+
+//Write MyBook class
+
+class MyBook : Book
+{
+    protected int price;
+
+    public MyBook(String t, String a, int p) : base(t, a)
+    {
+        price = p;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Author: {author}");
+        Console.WriteLine($"Price: {price}");
+    }
+}
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        String title = Console.ReadLine();
+        String author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
