5c6737de5aec1876dd0960aadb09db45d1841249
IvayloTodorov <rakuuqwas300@gmail.com>
2023-02-13 12:58:27 +0200

    MoreExceptions HackerRank

---
+++ HackeRankExersices/MoreExeptions/MoreExeptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+//Write your code here
+
+class Calculator
+{
+    public int power(int n, int p)
+    {
+        if (n < 0 || p < 0)
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+
+        double ans = Math.Pow(n, p);
+
+        return (int)ans;
+    }
+}
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        Calculator myCalculator = new Calculator();
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            string[] num = Console.ReadLine().Split();
+            int n = int.Parse(num[0]);
+            int p = int.Parse(num[1]);
+            try
+            {
+                int ans = myCalculator.power(n, p);
+                Console.WriteLine(ans);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+
+
+    }
+}
