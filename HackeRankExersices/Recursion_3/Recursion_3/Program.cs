d196308044f790d5c1a5d6c91d31592b1d62b4ab
IvayloTodorov <rakuuqwas300@gmail.com>
2023-02-26 17:24:38 +0200

    HackerRank Recursion 3

---
+++ HackeRankExersices/Recursion_3/Recursion_3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n)
+    {
+        int num = n;
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            num = num * i;
+        }
+
+        return num;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
